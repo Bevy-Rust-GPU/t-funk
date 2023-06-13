a40998cde49560512af0cbc79514263f219248b7
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-06-13 02:40:05 +0100

    Implement Remove, Drop, Empty, UnionWith, Union, SubtractFrom, Subtraction traits for Set

--- src/collection/set.rs
+++ src/collection/set.rs
@@ -2,37 +2,8 @@
 
 use crate::{
     closure::{Closure, Compose, Composed, Curried2, Curry2, Flip, Flipped},
-    macros::functions,
+    macros::{functions, types},
 };
-
-#[functions]
-pub trait Set<T> {
-    fn set(self, t: T) -> Self;
-}
-
-impl<T, A, B> Set<(A, B)> for T
-where
-    T: Clone + Set<A> + Set<B>,
-{
-    fn set(self, (a, b): (A, B)) -> Self {
-        self.set(a).set(b)
-    }
-}
-
-impl<T, A, B, C> Set<(A, B, C)> for T
-where
-    T: Clone + Set<A> + Set<B> + Set<C>,
-{
-    fn set(self, (a, b, c): (A, B, C)) -> Self {
-        self.set(a).set(b).set(c)
-    }
-}
-
-impl<T> Set<()> for T {
-    fn set(self, (): ()) -> Self {
-        self
-    }
-}
 
 #[functions]
 pub trait Get<T> {
@@ -60,6 +31,175 @@ where
 {
     fn get(self) -> (A, B, C) {
         (self.clone().get(), self.clone().get(), self.clone().get())
+    }
+}
+
+#[functions]
+#[types]
+pub trait Insert<T> {
+    type Insert;
+
+    fn insert(self, t: T) -> Self::Insert;
+}
+
+impl<T> Insert<()> for T {
+    type Insert = Self;
+
+    fn insert(self, (): ()) -> Self::Insert {
+        self
+    }
+}
+
+impl<T, A, B> Insert<(A, B)> for T
+where
+    T: Insert<A>,
+    InsertT<T, A>: Insert<B>,
+{
+    type Insert = InsertT<InsertT<T, A>, B>;
+
+    fn insert(self, (a, b): (A, B)) -> Self::Insert {
+        self.insert(a).insert(b)
+    }
+}
+
+impl<T, A, B, C> Insert<(A, B, C)> for T
+where
+    T: Insert<A>,
+    InsertT<T, A>: Insert<B>,
+    InsertT<InsertT<T, A>, B>: Insert<C>,
+{
+    type Insert = InsertT<InsertT<InsertT<T, A>, B>, C>;
+
+    fn insert(self, (a, b, c): (A, B, C)) -> Self::Insert {
+        self.insert(a).insert(b).insert(c)
+    }
+}
+
+#[functions]
+#[types]
+pub trait Remove<T> {
+    type Remove;
+    fn remove(self) -> (Self::Remove, T);
+}
+
+impl<T> Remove<()> for T {
+    type Remove = T;
+
+    fn remove(self) -> (Self::Remove, ()) {
+        (self, ())
+    }
+}
+
+impl<T, A, B> Remove<(A, B)> for T
+where
+    T: Remove<A>,
+    RemoveT<T, A>: Remove<B>,
+{
+    type Remove = RemoveT<RemoveT<T, A>, B>;
+
+    fn remove(self) -> (Self::Remove, (A, B)) {
+        let this = self;
+        let (this, a) = Remove::<A>::remove(this);
+        let (this, b) = Remove::<B>::remove(this);
+        (this, (a, b))
+    }
+}
+
+impl<T, A, B, C> Remove<(A, B, C)> for T
+where
+    T: Remove<A>,
+    RemoveT<T, A>: Remove<B>,
+    RemoveT<RemoveT<T, A>, B>: Remove<C>,
+{
+    type Remove = RemoveT<RemoveT<RemoveT<T, A>, B>, C>;
+
+    fn remove(self) -> (Self::Remove, (A, B, C)) {
+        let this = self;
+        let (this, a) = Remove::<A>::remove(this);
+        let (this, b) = Remove::<B>::remove(this);
+        let (this, c) = Remove::<C>::remove(this);
+        (this, (a, b, c))
+    }
+}
+
+#[functions]
+#[types]
+pub trait Drop<U> {
+    type Drop;
+
+    fn drop(self) -> Self::Drop;
+}
+
+impl<T, U> Drop<U> for T
+where
+    T: Remove<U>,
+{
+    type Drop = RemoveT<T, U>;
+
+    fn drop(self) -> Self::Drop {
+        self.remove().0
+    }
+}
+
+#[functions]
+#[types]
+pub trait Empty {
+    type Empty;
+
+    fn empty() -> Self::Empty;
+}
+
+#[functions]
+#[types]
+pub trait UnionWith<U> {
+    type UnionWith;
+
+    fn union_with(self, u: U) -> Self::UnionWith;
+}
+
+#[functions]
+#[types]
+pub trait Union<U> {
+    type Union;
+
+    fn union(self, u: U) -> Self::Union;
+}
+
+impl<T, U> Union<U> for T
+where
+    U: UnionWith<T>,
+{
+    type Union = UnionWithT<U, T>;
+
+    fn union(self, u: U) -> Self::Union {
+        u.union_with(self)
+    }
+}
+
+#[functions]
+#[types]
+pub trait SubtractFrom<U> {
+    type SubtractFrom;
+
+    fn subtract_from(self, u: U) -> Self::SubtractFrom;
+}
+
+#[functions]
+#[types]
+pub trait Subtraction<U> {
+    type Subtraction;
+
+    fn subtraction(self, u: U) -> Self::Subtraction;
+}
+
+impl<T, U> Subtraction<U> for T
+where
+    U: SubtractFrom<T>,
+{
+    type Subtraction = SubtractFromT<U, T>;
+
+    fn subtraction(self, u: U) -> Self::Subtraction {
+        u.subtract_from(self)
     }
 }
 
@@ -99,10 +239,10 @@ impl<T, U, O> LiftSet<U, O> for T
 where
     T: Closure<U, Output = O>,
 {
-    type LiftSet = Composed<Curried2<Flipped<SetF>>, T>;
+    type LiftSet = Composed<Curried2<Flipped<InsertF>>, T>;
 
     fn lift_set(self) -> Self::LiftSet {
-        self.compose_l(SetF.flip().curry2())
+        self.compose_l(InsertF.flip().curry2())
     }
 }
 
@@ -116,12 +256,12 @@ pub trait LiftContext<U> {
 pub type LiftContextT<T, U> = <T as LiftContext<U>>::LiftContext;
 
 impl<T, U> LiftContext<U> for T {
-    type LiftContext = Composed<Curried2<Flipped<SetF>>, Composed<T, GetF<U>>>;
+    type LiftContext = Composed<Curried2<Flipped<InsertF>>, Composed<T, GetF<U>>>;
 
     fn lift_context(self) -> Self::LiftContext {
         GetF::<U>::default()
             .compose_l(self)
-            .compose_l(SetF.flip().curry2())
+            .compose_l(InsertF.flip().curry2())
     }
 }
 
@@ -161,26 +301,34 @@ mod test {
         }
     }
 
-    impl Set<i32> for Context {
-        fn set(self, t: i32) -> Self {
+    impl Insert<i32> for Context {
+        type Insert = Self;
+
+        fn insert(self, t: i32) -> Self {
             Context { int: t, ..self }
         }
     }
 
-    impl Set<f32> for Context {
-        fn set(self, t: f32) -> Self {
+    impl Insert<f32> for Context {
+        type Insert = Self;
+
+        fn insert(self, t: f32) -> Self {
             Context { float: t, ..self }
         }
     }
 
-    impl Set<char> for Context {
-        fn set(self, t: char) -> Self {
+    impl Insert<char> for Context {
+        type Insert = Self;
+
+        fn insert(self, t: char) -> Self {
             Context { char: t, ..self }
         }
     }
 
-    impl Set<&'static str> for Context {
-        fn set(self, t: &'static str) -> Self {
+    impl Insert<&'static str> for Context {
+        type Insert = Self;
+
+        fn insert(self, t: &'static str) -> Self {
             Context { string: t, ..self }
         }
     }
@@ -204,10 +352,10 @@ mod test {
         assert_eq!(char, '3');
         assert_eq!(string, "4");
 
-        let ctx = Set::<i32>::set(ctx, 2);
-        let ctx = Set::<f32>::set(ctx, 3.0);
-        let ctx = Set::<char>::set(ctx, '4');
-        let ctx = Set::<&'static str>::set(ctx, "5");
+        let ctx = Insert::<i32>::insert(ctx, 2);
+        let ctx = Insert::<f32>::insert(ctx, 3.0);
+        let ctx = Insert::<char>::insert(ctx, '4');
+        let ctx = Insert::<&'static str>::insert(ctx, "5");
 
         assert_eq!(ctx.int, 2);
         assert_eq!(ctx.float, 3.0);
